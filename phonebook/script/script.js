69871f26c4dfa9872a561df193c43d1813a01633
Alex Din <din366@gmail.com>
2022-02-17 21:57:50 +0300

    fix getStorage func

--- phonebook/script/script.js
+++ phonebook/script/script.js
@@ -215,14 +215,16 @@
 
   /* Storage functions */
 
-  const getStorage = (key) => {
+  /* const getStorage = (key) => {
     const localStorageKey = localStorage.getItem(key);
     if (localStorageKey === null) {
       return [];
     } else {
       return JSON.parse(localStorageKey);
     }
-  };
+  }; */
+
+  const getStorage = (key) => JSON.parse(localStorage.getItem(key)) || [];
 
   const setStorage = (key, obj) => {
     const localStorageKey = getStorage(key);
